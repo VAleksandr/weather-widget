f75ec4219e12118eab5d61a878a3436a1bb434ef
Aleksandr <vlasovgomel@gmail.com>
2023-06-21 12:25:14 +0300

    add APIservice.js creat render.js and utils.js

--- scripts/modules/utils.js
+++ scripts/modules/utils.js
@@ -1,3 +1,5 @@
+const addZero = (n) => n<10 ? `0${n}` : n;
+
 export const getCurrentDateTime = () => {
     const months = [
         'янв',
@@ -29,18 +31,8 @@ export const getCurrentDateTime = () => {
     const month = months[date.getMonth()];
     const year = date.getFullYear();
     const dayOfWeek = weekday[date.getDay()];
-
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-
-    if (hours < 10) {
-        hours = `0${hours}`;
-    }
-
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-    }
-
+    const hours = addZero(date.getHours());
+    const minutes = addZero(date.getMinutes());
 
     return {dayOfMonth, month, year, hours, minutes, dayOfWeek};
 };
